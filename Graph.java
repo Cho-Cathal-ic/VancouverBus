3f90a7f99de5b17063f033884f8728352d3c57c0
Cho-Cathal-ic <68115012+Cho-Cathal-ic@users.noreply.github.com>
2022-04-10 15:27:44 +0100

    Creates Graph with Stops and Stop Times - Working

--- Graph.java
+++ Graph.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 class Stop{
@@ -10,14 +8,20 @@ class Stop{
 		String zone_id;
 		int location_type;
 		int parent_station;
-		Map <Stop, Double> adjacentStops = new HashMap<>();
-		
+		ArrayList<StopTime> destinations = new ArrayList<StopTime>();
+
 		public Stop(String input) {
 			Scanner scan = new Scanner(input);
 			scan.useDelimiter(",");
 			
 			id = scan.nextInt();
-			code = scan.nextInt();
+			if(scan.hasNextInt()) {
+				code = scan.nextInt();
+			}
+			else {
+				code = -1;
+				scan.next();
+			}
 			name = scan.next();
 			desc = scan.next();
 			lon = scan.nextDouble();
@@ -25,16 +29,19 @@ class Stop{
 			zone_id = scan.next();
 			scan.next();
 			location_type = scan.nextInt();
-			parent_station = scan.nextInt();
 			
+			if (scan.hasNextInt()){
+				parent_station = scan.nextInt();
+			}
 			scan.close();
 			
-			adjacentStops = new HashMap<>();
 		}
 		
-		public void addEdge() {
-			
+		public void addDestination(StopTime st) {
+			destinations.add(st);
 		}
+		
+		//connections
 		
 		public String toText() {
 			return "" + id + ", " + code + ", " + name + ", " + desc + ", " + lon + ", " + lat + ", " + 
@@ -73,6 +80,74 @@ class Stop{
 			return parent_station;
 		}
 	}
+
+	class StopTime{
+		//trip_id,arrival_time,departure_time,stop_id,stop_sequence,stop_headsign,pickup_type,drop_off_type,shape_dist_traveled
+		int trip_id;
+		int arrival_time, departure_time;
+		int stop_id;
+		int stop_headsign;
+		int pickup_type;
+		int drop_off_type;
+		double shape_dist_traveled;
+		
+		public StopTime(String input){
+			Scanner scan = new Scanner(input);
+			scan.useDelimiter(",");
+			
+			trip_id = scan.nextInt();
+			arrival_time = TimetoSeconds(scan.next());
+			departure_time = TimetoSeconds(scan.next());
+			stop_id = scan.nextInt();
+			stop_headsign = scan.nextInt();
+			
+			if(scan.hasNextInt()) {pickup_type = scan.nextInt();}
+			else {
+				pickup_type = -1;
+				scan.next();
+			}
+			drop_off_type = scan.nextInt();
+			
+			scan.close();
+		}
+		
+		public int TimetoSeconds(String input) {
+			
+			Scanner scan = new Scanner(input);
+			scan.useDelimiter(":");
+			int hours = 0, minutes = 0, seconds = 0;
+			if(scan.hasNextInt()) {hours = scan.nextInt();}
+			if(scan.hasNextInt()) {minutes = scan.nextInt();}
+			if(scan.hasNextInt()) {seconds = scan.nextInt();}
+			scan.close();
+			
+			return ((hours*3600) + (minutes*60) + seconds);
+		}
+		
+	}
+	
+	class Transfer{
+		int from;
+		int to;
+		int type;
+		int minTransferTime;
+		
+		Transfer(String input){
+			Scanner scan = new Scanner(input);
+			scan.useDelimiter(":");
+			
+			from = scan.nextInt();
+			to = scan.nextInt();
+			type = scan.nextInt();
+			if(scan.hasNext()) {
+				minTransferTime = scan.nextInt();
+			}
+			else {
+				minTransferTime = 0;
+			}
+			scan.close();
+		}
+	}
 	
 	class Graph{
 		public ArrayList<Stop> stops;
@@ -86,5 +161,28 @@ class Stop{
 		
 		public void addStop(Stop vertex){
 			stops.add(vertex);
+		}
+		
+		public ArrayList<Stop> getStops() {
+			ArrayList<Stop> s = new ArrayList<Stop>();
+			
+			for(int i=0; i<stops.size(); i++) {
+				s.add(stops.get(i));
+			}
+			
+			return s;
+		}
+		
+		public Stop findStop(int id) {
+			Stop s = null;
+			
+			for(int i=0; i<stops.size(); i++) {
+				s = stops.get(i);
+				if(s.getId() == id) {
+					return s;
+				}
+			}
+			
+			return null;
 		}
 	}
--- VancouverBus.java
+++ VancouverBus.java
@@ -11,14 +11,49 @@ public class VancouverBus {
 		
 		Graph graph = new Graph(g);
 		
-		File f = new File("stops.txt");
+		File f = new File("src\\stops.txt");
 		Scanner scan = new Scanner(f);
+		
+		scan.nextLine();
 		
 		while(scan.hasNextLine()){
 			Stop s = new Stop(scan.nextLine());
 			graph.addStop(s);
 		}
 		scan.close();
+		
+		File h = new File("src\\stop_times.txt");
+		Scanner journeys = new Scanner(h);
+		
+		journeys.nextLine();
+		
+		ArrayList <StopTime> stopTime = new ArrayList<StopTime>();
+		
+		while(journeys.hasNextLine()) {
+			StopTime j = new StopTime(journeys.nextLine());
+			stopTime.add(j);
+		}
+		
+		for (int i = 0; i < stopTime.size(); i++) {
+			for (int j = i+1; j < stopTime.size(); j++) {
+				StopTime stopI = stopTime.get(i);
+				StopTime stopJ = stopTime.get(j);
+				
+				if(stopI.trip_id == stopJ.trip_id) {
+					if(stopI.arrival_time < stopJ.arrival_time) {
+						Stop s = graph.findStop(stopI.stop_id);
+						s.addDestination(stopJ);
+					}
+					else {
+						Stop s = graph.findStop(stopJ.stop_id);
+						s.addDestination(stopI);
+					}
+				}
+			}
+		}
+		
+		journeys.close();
+		
 		
 		
 	}
